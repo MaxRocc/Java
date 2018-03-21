02317998fc5f7bf64905d37d6ce394b213570594
Maxime Roccisano <maxime.roccisano@campus-numerique-in-the-alps.com>
2018-03-21 13:52:04 +0100

    update ignoe file

---
+++ java-projet-1/src/main/java/Warrior.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Warrior {
+    String Name="Conan";
+    String Image;
+    int Life=120;
+    int AttackLevel=102;
+    String Weapon=["someSword", ];
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ java-projet-1/src/main/java/Wizard.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Wizard {
+    String Name="Rincewind";
+    String image;
+    int Life=20;
+    int AttackLevel=2;
+    String Spell="oneOfTheEightSpells";
+
+    public static void main(String[] args)
+    {
+
+    }
+}
